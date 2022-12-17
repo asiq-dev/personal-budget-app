050d9d8be1cb21640e7446ac6225d8dbae775727
asiqul islam <asiq.dev@gmail.com>
2022-12-17 22:03:48 +0600

    update project

--- app/src/main/java/com/example/mymoneymanagementapp/LoginActivity.java
+++ app/src/main/java/com/example/mymoneymanagementapp/LoginActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,11 +29,25 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
 
+    private FirebaseAuth.AuthStateListener authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if(user!=null){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
@@ -90,4 +105,19 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        mAuth.addAuthStateListener(authStateListener);
+//
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//
+//        mAuth.removeAuthStateListener(authStateListener);
+//    }
 }
--- app/src/main/java/com/example/mymoneymanagementapp/MainActivity.java
+++ app/src/main/java/com/example/mymoneymanagementapp/MainActivity.java
@@ -6,22 +6,63 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardView budgetCardView;
+//    private CardView budgetCardView, todayCardView;
+    private ImageView budgetBtnImageView, todayBtnImageView, weekBtnImageView,monthBtnImageView,analyticsImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        budgetCardView = findViewById(R.id.budgetCardView);
+        budgetBtnImageView = findViewById(R.id.budgetBtnImageView);
+        todayBtnImageView = findViewById(R.id.todayBtnImageView);
+        weekBtnImageView = findViewById(R.id.weekBtnImageView);
+        monthBtnImageView = findViewById(R.id.monthBtnImageView);
+        analyticsImageView = findViewById(R.id.analyticsImageView);
 
-        budgetCardView.setOnClickListener(new View.OnClickListener() {
+
+        budgetBtnImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        todayBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weekBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                intent.putExtra("type","week");
+                startActivity(intent);
+            }
+        });
+
+        monthBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                intent.putExtra("type","month");
+                startActivity(intent);
+            }
+        });
+
+        analyticsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseAnalyticActivity.class);
                 startActivity(intent);
             }
         });
